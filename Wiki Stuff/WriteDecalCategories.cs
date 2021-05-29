39fbb61b44a6d384cd7c523e6779ab954acea457
Gregory Brutsche <gregory@mail.brutsches.local>
2021-05-29 18:17:32 -0400

    Add decal caregory writer.

---
+++ Wiki Stuff/WriteDecalCategories.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using Base_Mod.Models;
+using JetBrains.Annotations;
+
+namespace Wiki_Writer.Wiki_Stuff {
+    public static class WriteDecalCategories {
+        [OnIslandSceneLoaded]
+        [UsedImplicitly]
+        public static void Go() {
+            var msg = Plugin.GetHeader() +
+                      "Name | AssetId (GUID) | Type\r\n" +
+                      "--- | --- | ---\r\n";
+
+            foreach (var item in (from dResource in RuntimeAssetStorage.GetAssets<DecalResource>()
+                                  select dResource.Category).Distinct()) {
+                msg += $"{Plugin.GetName(item.name)} | {item.AssetId} | {item.GetType()}\r\n";
+            }
+
+            File.WriteAllText(Plugin.BASE_OUTPUT_PATH + "Decal Category Ids.txt", msg);
+        }
+    }
+}
