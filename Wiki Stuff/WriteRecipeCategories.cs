505081859448a2b22edd6b59032d49ac531f45ba
Gregory Brutsche <gregory@mail.brutsches.local>
2021-05-29 18:17:53 -0400

    Add recipe category writers.

---
+++ Wiki Stuff/WriteRecipeCategories.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using Base_Mod.Models;
+using JetBrains.Annotations;
+
+namespace Wiki_Writer.Wiki_Stuff {
+    public static class WriteRecipeCategories {
+        [OnIslandSceneLoaded]
+        [UsedImplicitly]
+        public static void Go() {
+            var msg = new StringBuilder(Plugin.GetHeader())
+                      .Append("Name | Category (s)\r\n")
+                      .Append("--- | ---\r\n");
+
+            foreach (var recipe in GameResources.Instance.Recipes.OrderBy(i => i.name)) {
+                var categoryNames    = recipe.Categories.Select(category => category.name);
+                var categoryListText = categoryNames == null || categoryNames.Length == 0 ? "{null}" : categoryNames.JoinString();
+
+                msg.Append($"{Plugin.GetName(recipe.name)} | {categoryListText}\r\n");
+            }
+
+            File.WriteAllText(Plugin.BASE_OUTPUT_PATH + "Recipe Categories.txt", msg.ToString());
+        }
+    }
+}
---
+++ Wiki Stuff/WriteRecipeCategoryIds.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using Base_Mod.Models;
+using JetBrains.Annotations;
+
+namespace Wiki_Writer.Wiki_Stuff {
+    public static class WriteRecipeCategoryIds {
+        [OnIslandSceneLoaded]
+        [UsedImplicitly]
+        public static void Go() {
+            var msg = new StringBuilder(Plugin.GetHeader());
+
+            var categoryNameList = (from recipe in GameResources.Instance.Recipes
+                                    where recipe.Categories != null && recipe.Categories.Length > 0
+                                    from category in recipe.Categories
+                                    where !string.IsNullOrEmpty(category.name)
+                                    orderby category.name
+                                    select category.name).Distinct();
+
+            foreach (var name in categoryNameList) {
+                msg.Append($" - {Plugin.GetName(name)}\r\n");
+            }
+
+            File.WriteAllText(Plugin.BASE_OUTPUT_PATH + "Recipe Category Ids.txt", msg.ToString());
+        }
+    }
+}
